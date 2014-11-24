478256e4166b26849a4da0d6e0c4b3f2c7c77b60
himapriya777 <himapriya777@gmail.com>
2014-11-24 11:21:47 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,31 @@ function validateForm() {
             alert(output);
     });
 }
+
+function send_post() {
+	 if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    hr=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    hr=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+      
+      
+       var hr = new XMLHttpRequest();
+
+	var fn = document.getElementById("post").value;
+	var vars = "post="+fn;
+	var url ="send_post.php";
+	hr.open("POST",url,true);
+ hr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        hr.onreadystatechange = function() {
+        if(hr.readyState == 4 && hr.status == 200) {
+        		 	
+	document.getElementById("status").innerHTML = hr.responseText;
+		
+        }
+    }
+    hr.send(vars);
+	document.getElementById("status").innerHTML = "Processing...";
+}﻿
+
