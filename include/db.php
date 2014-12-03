89e666f1c4632cd239fc0c3a4da32d59905aad26
ecorra37 <ecorra37@gmail.com>
2014-12-02 18:53:34 -0700

    EC

--- include/db.php
+++ include/db.php
@@ -1,9 +1,9 @@
 <?php
 $currency='$';
 $hostname="localhost";
-$database="cs5339teamxfa14";
-$username="root";
-$password="";
+$database="cs5339team9fa14";
+$username="team9";
+$password="1234";
  $con=mysqli_connect($hostname,$username,$password,$database);
 // Check connection
 if (mysqli_connect_errno()) {
--- include/dbEC.php
+++ include/dbEC.php
@@ -1,16 +1,18 @@
-<?php
-$currency='$';
-$hostname="localhost";
-$database="cs5339teamxfa14";
-$username="root";
-$password="";
- $con=mysqli_connect($hostname,$username,$password,$database);
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-}
-//else
-//    echo "connected";
+<?PHP
+	require_once("./include/configSite.php");
 
+	$configSite = new ConfigSite();
+
+	$configSite->InitDB(/*hostname*/'localhost',
+						/*username*/'team9',
+						/*password*/'1234',
+						/*database name*/'cs5339team9fa14',
+						/*table name 1*/'admin',
+						/*table name 2*/'friends',
+						/*table name 3*/'friend_request',
+						/*table name 4*/'items',
+						/*table name 5*/'master',
+						/*table name 6*/'messages',
+						/*table name 7*/'students_master',
+						/*table name 8*/'users');
 ?>
