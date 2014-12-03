79a73fcd96ec0dd4b65c81003b51d79a9b2921e6
himapriya777 <himapriya777@gmail.com>
2014-12-02 19:59:27 -0700

    hk

---
+++ login/index.php
@@ -0,0 +1,76 @@
+ <?php
+ include('include/db.php');
+session_start();
+
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Alumni Website</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/main.js" ></script>
+
+
+
+    </head>
+    <body>
+    <div id="navigation_container">
+         <div class="rectangle">
+        <ul id="navigation">
+
+            <li><a href="index.php"><span id="highlight">Home</span></a></li>
+            <li><a href="Store.php">Store</a></li>
+            <li><a href="Find.php">Find Graduate</a></li>
+            <li><a href="About.php">About Us</a></li>
+                       <?php if ((isset($_SESSION['login_status']))) {
+?>
+
+            <li> <a href="loginprofile.php">Profile</a></li>
+              <li> <a href="signout.php">Sign Out</a></li>
+        </ul>
+    </div>
+<?php }
+else {
+	//do nothing
+?>
+</div>
+
+   
+
+<div>
+			<span id="login_input">
+	<form name="Login_Form" action="validate.php" onsubmit="return validateForm()" method="post">
+		User Name:<input type="text" name="username"><br>
+		Password:<input type="password" name="password"><br>
+		<input type="submit" value="Login" name="login_submit">
+		<p id="Message"></p>
+	</form>
+</span>
+			</div>
+            
+       <?php } ?>     
+   <div id="pageMiddle">
+            <p >
+
+            <h3>Welcome to UTEP CS Alumni Association.</h3>
+
+</p>
+<img src="img/CS-Promo-Building.jpg" alt="utep cs" width="500" align="middle"/>
+           
+            
+        </div>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        
+        
+    </body>
+</html>
