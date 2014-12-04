972d6ae9188a23bf01ef99610c42fd92c950e1c5
erickegm <erickegm@gmail.com>
2014-12-04 00:00:35 -0700

    EGM
    Hima, This is the main ajax file. It calls ajax_edit_profile_pic.php.....

---
+++ ajax_upload_pic.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<script>
+			function hyperlink(){
+				var b;
+				if(window.XMLHttpRequest){
+					b = new XMLHttpRequest();
+				}
+				else {
+					b = new ActiveXObject("Microsoft.HMLHTTP");
+				}
+				b.onreadystatechange = function(){
+					if (b.readyState==4 && b.status==200){
+						document.getElementById('link').innerHTML = b.responseText;
+					}
+				}
+				b.open('GET', 'ajax_edit_profile_pic.php', true);
+				b.send();
+			}
+		</script>
+	</head>
+	<body>
+		<h3>Click picture to edit!</h3>
+		<a id='link'><img  onclick='hyperlink()' src='pictures/welcome.png' width='60' height='50' ></a>
+		<span id='change'></span>
+	</body>
+</html>
