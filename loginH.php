59ec42852ec600223a58854b9653b421f374400f
ecorra37 <ecorra37@gmail.com>
2015-06-15 20:48:00 -0500

    EC

---
+++ loginH.php
@@ -0,0 +1,6 @@
+<form name="Login_Form" action="validate.php" onsubmit="return validateForm()" method="post">
+	User Name:<input type="text" name="username"><br>
+	Password:<input type="password" name="password"><br>
+	<input type="submit" value="Login" name="login_submit">
+	<p id="Message"></p>
+</form>
